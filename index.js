b82c5485065042dcbb26d552dcc2ce055fb375b5
solo-mizan <mizanbd.eee@gmail.com>
2022-05-05 00:03:55 +0600

    delete method added

--- index.js
+++ index.js
@@ -35,6 +35,22 @@ async function run() {
             const service = await serviceCollection.findOne(query);
             res.send(service);
         });
+
+        //POST
+        app.post('/service', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+
+        })
+
+        //DELETE
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
 
