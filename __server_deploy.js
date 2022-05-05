90aa25759f65b2ba840534a27934331673315fe0
solo-mizan <mizanbd.eee@gmail.com>
2022-05-06 02:34:52 +0600

    script updated

--- __server_deploy.js
+++ __server_deploy.js
@@ -6,11 +6,20 @@
  * 2. verify email
  * 3. install heroku CLI
  * 4. heroku login command
- * --------------------------
+ * ------------------------------------------------------------------------------------
  * FOR EACH PROJECT ONE TIME
- * --------------------------
+ * ------------------------------------------------------------------------------------
  * 1. heroku create
  * 2. update git add . , git commit , git push
  * 3. git push heroku main
+ * 4. Go to heroku Dashboard > Current Project > Settings > Reveal Config Vars
+ * 5. Copy paste config vars from your .env file from local computer
+ * 6. Make sure you have white listed all ip in mongodb
+ * --------------------------------
+ * CONNECT SERVER WITH CLIENT SIDE 
+ * --------------------------------
+ * replace localhost by heroku url
+ * npm run build
+ * firebase or netlify deploy
  * 
  */
