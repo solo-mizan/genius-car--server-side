8a32957a183d8eaa6d70887c88d63b27dcccba89
solo-mizan <mizanbd.eee@gmail.com>
2022-05-06 01:18:22 +0600

    heroku deploy steps added

---
+++ __server_deploy.js
@@ -0,0 +1,16 @@
+/**
+ * -------------------------
+ * ONE TIME FOR MY COMPURER
+ * -------------------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku CLI
+ * 4. heroku login command
+ * --------------------------
+ * FOR EACH PROJECT ONE TIME
+ * --------------------------
+ * 1. heroku create
+ * 2. update git add . , git commit , git push
+ * 3. git push heroku main
+ * 
+ */
